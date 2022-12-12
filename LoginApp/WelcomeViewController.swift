db2e2bc82c6fa7fd618957534aaf432121defab2
voronovski <avoronovski@gmail.com>
2022-12-12 00:25:15 -0800

    small code optimization

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
+final class WelcomeViewController: UIViewController {
     
     // MARK: - IB Outlets
     @IBOutlet var welcomeLabel: UILabel!
